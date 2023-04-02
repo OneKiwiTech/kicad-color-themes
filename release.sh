0221882e9dc9166864f6779b3368261672b6e134
Van Son <thanh.duong.vs@gmail.com>
2023-04-02 20:49:18 +0700

    update metadata

--- release.sh
+++ release.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 cd kiwi-light
+cp template.json metadata.json
+sed -i '/download_/d' metadata.json
+sed -i '/install_size/d' metadata.json
+
 zip -r kiwi-light.zip colors resources metadata.json
 mv kiwi-light.zip vn.onekiwi.theme-kiwi-light.zip
-cd ../
+
+sha=$(sha256sum vn.onekiwi.theme-kiwi-light.zip | cut -d' ' -f1)
+size=$(du -b vn.onekiwi.theme-kiwi-light.zip | cut -f1)
+installSize=$(unzip -l vn.onekiwi.theme-kiwi-light.zip | tail -1 | xargs | cut -d' ' -f1)
+
+cp template.json metadata.json
+sed -i -e "s/SHA256/$sha/g" metadata.json
+sed -i -e "s/DOWNLOAD_SIZE/$size/g" metadata.json
+sed -i -e "s/INSTALL_SIZE/$installSize/g" metadata.json
+
+ls -lh vn.onekiwi.theme-kiwi-light.zip metadata.json
